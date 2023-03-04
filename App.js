251a997f6a8f17fd468fb77df2d9e3fd229f9550
codydon <donaldonserio20@gmail.com>
2023-03-04 09:40:05 +0300

    toggling fav

--- App.js
+++ App.js
@@ -4,19 +4,19 @@ const app = Vue.createApp({
     // template: '<h2> I am the template </h2>'
     data() {
         return {
-            url:'www.unsplash.com',
+            url: 'www.unsplash.com',
             showBooks: true,
             title: 'Atomic habits',
             author: 'James Clear',
             age: 52,
             x: 0,
             y: 0,
-            books : [
-                { author: 'J.K. Rowling', title: 'Harry Potter and the Philosopher\'s Stone', img: 'assets/1.png', isFav:true},
-                { author: 'Harper Lee', title: 'To Kill a Mockingbird', img: 'assets/2.png', isFav:false},
-                { author: 'J.R.R. Tolkien', title: 'The Lord of the Rings', img: 'assets/3.png', isFav:true}
-             ]
-            
+            books: [
+                { author: 'J.K. Rowling', title: 'Harry Potter and the Philosopher\'s Stone', img: 'assets/1.png', isFav: true },
+                { author: 'Harper Lee', title: 'To Kill a Mockingbird', img: 'assets/2.png', isFav: false },
+                { author: 'J.R.R. Tolkien', title: 'The Lord of the Rings', img: 'assets/3.png', isFav: true }
+            ]
+
         }
     },
 
@@ -30,15 +30,18 @@ const app = Vue.createApp({
             this.showBooks = !this.showBooks
         },
 
-        handleEvent(e, data){
+        handleEvent(e, data) {
             console.log(e, e.type)
-            if (data){
+            if (data) {
                 console.log(data)
             }
         },
-        handleMousemove(e){
+        handleMousemove(e) {
             this.x = e.offsetX
             this.y = e.offsetY
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
         }
     }
 
