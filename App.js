d3991e04b515e33bdf560a9f3030a6a22e8e431f
codydon <donaldonserio20@gmail.com>
2023-03-04 05:52:19 +0300

    conditional rendering

--- App.js
+++ App.js
@@ -2,20 +2,26 @@ const app = Vue.createApp({
     //this creates a vue app
     //data, function
     // template: '<h2> I am the template </h2>'
-    data () {
+    data() {
         return {
+            showBooks: true,
             title: 'Atomic habits',
             author: 'James Clear',
             age: 52
         }
-    }, 
-    methods:{
-        changeTitle(title){
+    },
+
+    methods: {
+        changeTitle(title) {
             // this.title = "Things fall apart"
             this.title = title
+        },
+
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
         }
     }
 
-}) 
+})
 
 app.mount('#app') // mounts to element of app id
